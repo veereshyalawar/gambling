02103ba30f8888f040098b0c59921fce7a7d2c2b
veeresh <veereshyalawar2@gmail.com>
2022-02-18 19:21:21 -0800

    added uc1 file

---
+++ initial_amount.java
@@ -0,0 +1,16 @@
+package gambler;
+
+import java.util.Scanner;
+
+public class initial_amount {
+
+	public static void main(String[] args) {
+    Scanner scanner = new scanner(System.in); 
+	System.out.println("enter the amount");
+	
+	String amount = scanner.nextLine();
+	System.out.println("amount :" + amount);
+
+	}
+
+}
